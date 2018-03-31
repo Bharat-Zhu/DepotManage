dcb20b3f7c2f332e491688540003a0deac520785
Bharat-Zhu <269475408@qq.com>
2018-03-31 21:04:22 +0800

    配置文件
    2018/3/31 Spring MVC的配置文件

---
+++ src/main/java/com/depot/controller/LoginController.java
@@ -0,0 +1,11 @@
+package com.depot.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class LoginController {
+
+    public void loginCheck() {
+        
+    }
+}
